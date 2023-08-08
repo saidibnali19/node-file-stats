52e8787314d8a0dd2cd675ff7b3cb639f0ad270f
Said Ali <saidibnali19@gmail.com>
2023-08-08 12:31:14 +0300

    feat: extracted more information about a file using methods and properties of the stats object

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const fs = require("fs")
+const fs = require("fs");
 
 fs.stat("test.txt", (error, stats) => {
-    if (error) console.error(error)
-    console.log(stats)
-})
+  if (error) console.error(error);
+  console.log(stats);
+  console.log(stats.isFile());
+  console.log(stats.isDirectory())
+  console.log(stats.isSymbolicLink())
+  console.log(stats.size)
+});
